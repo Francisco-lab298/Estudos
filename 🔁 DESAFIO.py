d1d1c22f102d2de4f5889650d2aa49d884063c86
Francisco-lab298 <francisschmidt2026@gmail.com>
2026-02-11 23:00:51 -0300

    comentado

--- 🔁 DESAFIO.py
+++ 🔁 DESAFIO.py
@@ -1,6 +1,6 @@
-pessoas = []
+pessoas = []# O código acima cria uma lista vazia chamada "pessoas". Em seguida, ele solicita ao usuário que insira o nome e a idade de três pessoas, armazenando essas informações em dicionários. Cada dicionário é adicionado à lista "pessoas". Por fim, o código imprime o nome e a idade de cada pessoa na lista, bem como a lista completa de pessoas.
 
-usuario = {}
+usuario = {}# O código acima cria um dicionário vazio chamado "usuario". Em seguida, ele solicita ao usuário que insira seu nome e idade, armazenando essas informações no dicionário. O dicionário é então adicionado à lista "pessoas".
 usuario["nome"] = input("Digite seu nome: ")
 usuario["idade"] = int(input("Digite sua idade: "))
 
@@ -19,4 +19,4 @@ pessoas.append(usuario2)
 for pessoa in pessoas:
     print(pessoa["nome"], pessoa["idade"])
     
-print(pessoas)
+print(pessoas)# O código acima cria uma lista chamada "pessoas" e adiciona três dicionários, cada um representando um usuário com seu nome e idade. Em seguida, o código imprime o nome e a idade de cada pessoa na lista, bem como a lista completa de pessoas.
