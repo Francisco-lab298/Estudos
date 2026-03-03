1f89cbb7cff25eb4dc61c30c86e661b719313479
Francisco-lab298 <francisschmidt2026@gmail.com>
2026-03-02 22:46:04 -0300

    Cadastro

--- desafio1.py
+++ desafio1.py
@@ -1,9 +1,9 @@
-cadastro = []
+cadastro = []# Lista para armazenar os nomes cadastrados
 
-for i in range(5):
-    nome = input("Digite seu nome: ")
-    cadastro.append(nome)
+for i in range(5):# Loop para solicitar o cadastro de 5 nomes
+    nome = input("Digite seu nome: ")# Solicita ao usuário que digite seu nome
+    cadastro.append(nome)# Adiciona o nome digitado à lista de cadastro
     
-print("Nomes cadastrados:")
-for nome in cadastro:
-    print(nome)
+print("Nomes cadastrados:")# Imprime uma mensagem indicando que os nomes cadastrados serão exibidos
+for nome in cadastro:# Loop para percorrer a lista de cadastro e imprimir cada nome
+    print(nome)# Imprime o nome atual da lista de cadastro
