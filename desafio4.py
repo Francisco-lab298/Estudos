52f9b99dfdde5e0007754b1b36da4a4fec507a34
Francisco-lab298 <francisschmidt2026@gmail.com>
2026-03-10 23:07:05 -0300

    for/while

--- desafio4.py
+++ desafio4.py
@@ -1,12 +1,12 @@
-for numero in range(1, 11):
-    print(numero)
+for numero in range(1, 11):# Itera sobre os números de 1 a 10 (inclusive) e imprime cada número
+    print(numero)# Imprime o número atual na iteração
     
 
-numero =1
-while numero <= 10:
-    print(numero)
-    numero += 1
+numero =1# Inicializa a variável numero com o valor 1
+while numero <= 10:# Continua a execução do loop enquanto numero for menor ou igual a 10
+    print(numero)# Imprime o valor atual de numero
+    numero += 1# Incrementa o valor de numero em 1 a cada iteração do loop
     
     
-for numero in range(2, 21, 2):
-        print(numero)
+for numero in range(2, 21, 2):# Itera sobre os números de 2 a 20 (inclusive) com um passo de 2, ou seja, imprime apenas os números pares
+        print(numero)# Imprime o número atual na iteração, que será um número par entre 2 e 20
