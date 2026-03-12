a5e784853925cabf8ee4d06bea586ab58f0af75c
Francisco-lab298 <francisschmidt2026@gmail.com>
2026-03-11 22:50:19 -0300

    calculadora

--- desafio5.py
+++ desafio5.py
@@ -1,10 +1,10 @@
-Numero1 = int(input("Digite um numero inteiro: "))
-Numero2 = int(input("Digite outro numero inteiro: "))
+Numero1 = int(input("Digite um numero inteiro: "))# Solicita ao usuário que digite um número inteiro e armazena o valor na variável Numero1
+Numero2 = int(input("Digite outro numero inteiro: "))# Solicita ao usuário que digite outro número inteiro e armazena o valor na variável Numero2
 
-print("Soma:", Numero1 + Numero2)
-print("Subtração:", Numero1 - Numero2)
-print("Multiplicação:", Numero1 * Numero2)
-print("Divisão:", Numero1 / Numero2)
-print("Resto da divisão:", Numero1 % Numero2)
-print("Divisão inteira:", Numero1 // Numero2)
-print("Potência:", Numero1 ** Numero2)
+print("Soma:", Numero1 + Numero2)# Imprime a soma dos dois números, calculada pela expressão Numero1 + Numero2
+print("Subtração:", Numero1 - Numero2)# Imprime a subtração dos dois números, calculada pela expressão Numero1 - Numero2
+print("Multiplicação:", Numero1 * Numero2)# Imprime a multiplicação dos dois números, calculada pela expressão Numero1 * Numero2
+print("Divisão:", Numero1 / Numero2)# Imprime a divisão dos dois números, calculada pela expressão Numero1 / Numero2. Note que esta operação pode resultar em um número decimal se os números não forem divisíveis.
+print("Resto da divisão:", Numero1 % Numero2)# Imprime o resto da divisão dos dois números, calculada pela expressão Numero1 % Numero2. Esta operação retorna o valor que sobra após a divisão inteira de Numero1 por Numero2.
+print("Divisão inteira:", Numero1 // Numero2)# Imprime a divisão inteira dos dois números, calculada pela expressão Numero1 // Numero2. Esta operação retorna o resultado da divisão sem considerar o resto, ou seja, apenas a parte inteira do resultado.
+print("Potência:", Numero1 ** Numero2)# Imprime a potência de Numero1 elevado a Numero2, calculada pela expressão Numero1 ** Numero2. Esta operação retorna o resultado de multiplicar Numero1 por ele mesmo Numero2 vezes.
