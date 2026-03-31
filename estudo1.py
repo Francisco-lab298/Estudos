cc9fcc366919964c517237180de8eed183da90e5
Francisco-lab298 <francisschmidt2026@gmail.com>
2026-03-30 23:27:58 -0300

    Reforço class

--- estudo1.py
+++ estudo1.py
@@ -1,23 +1,23 @@
-class Vendedor():
-        def __init__(self, nome):
-            self.nome = nome
-            self.vendas = 0
+class Vendedor():#
+        def __init__(self, nome):#
+            self.nome = nome#
+            self.vendas = 0#
             
-        def vendeu(self, vendas):
-            self.vendas = vendas
+        def vendeu(self, vendas):#
+            self.vendas = vendas#
             
-        def bateu_meta(self, meta):
-            if self.vendas > meta:
-                print(self.nome, "bateu a meta")
-            else:
-                print(self.nome, "não bateu a meta")
+        def bateu_meta(self, meta):#
+            if self.vendas > meta:#
+                print(self.nome, "bateu a meta")#
+            else:#
+                print(self.nome, "não bateu a meta")#
             
 
 
-vendedor1 = Vendedor("Chico")
-vendedor1.vendeu(1000)
-vendedor1.bateu_meta(600)
+vendedor1 = Vendedor("Chico")#
+vendedor1.vendeu(1000)#
+vendedor1.bateu_meta(600)#
 
-vendedor2 = Vendedor("cisco")
-vendedor2.vendeu(500)
-vendedor2.bateu_meta(600)
+vendedor2 = Vendedor("cisco")#
+vendedor2.vendeu(500)#
+vendedor2.bateu_meta(600)#
